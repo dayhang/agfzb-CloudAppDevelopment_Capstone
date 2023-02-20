0856a0dbc40ca1e76f61b9038949d6570520b389
dayhang <dheogks12@gmail.com>
2023-02-20 15:55:25 +0900

    changes

--- functions/get-dealership.js
+++ functions/get-dealership.js
@@ -1,5 +1,5 @@
 /**
- * Get all dealerships
+ * Get all dealerships or filtered dealerships by state
  */
 
 const { CloudantV1 } = require('@ibm-cloud/cloudant');
@@ -9,28 +9,51 @@ function main(params) {
 
     const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
     const cloudant = CloudantV1.newInstance({
-      authenticator: authenticator
+        authenticator: authenticator
     });
     cloudant.setServiceUrl(params.COUCH_URL);
-    
-    let dbname = "dealerships"
-    let dbListPromise = getAllRecords(cloudant, dbname);
+
+    // Parse state from query parameter
+    const state = params.state;
+
+    let dbListPromise;
+    if (state) {
+        // Get filtered dealerships by state
+        dbListPromise = getMatchingRecords(cloudant, 'dealerships', { state: state });
+    } else {
+        // Get all dealerships
+        dbListPromise = getAllRecords(cloudant, 'dealerships');
+    }
+
     return dbListPromise;
 }
 
-                        
- /*
- Sample implementation to get all the records in a db.
- */
- function getAllRecords(cloudant,dbname) {
-     return new Promise((resolve, reject) => {
-         cloudant.postAllDocs({ db: dbname, includeDocs: true, limit: 10 })            
-             .then((result)=>{
-               resolve({result:result.result.rows});
-             })
-             .catch(err => {
+function getMatchingRecords(cloudant, dbname, selector) {
+    return new Promise((resolve, reject) => {
+        cloudant.postFind({ db: dbname, selector: selector })
+            .then((result) => {
+                resolve({ result: result.result.docs });
+            })
+            .catch(err => {
                 console.log(err);
                 reject({ err: err });
-             });
-         })
- }
+            });
+    })
+}
+
+
+/*
+Sample implementation to get all the records in a db.
+*/
+function getAllRecords(cloudant, dbname) {
+    return new Promise((resolve, reject) => {
+        cloudant.postAllDocs({ db: dbname, includeDocs: true, limit: 10 })
+            .then((result) => {
+                resolve({ result: result.result.rows });
+            })
+            .catch(err => {
+                console.log(err);
+                reject({ err: err });
+            });
+    })
+}
